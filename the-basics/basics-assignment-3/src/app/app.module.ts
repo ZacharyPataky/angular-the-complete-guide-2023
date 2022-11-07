228d33c2698ceeaeda14711bb3e97131b3e6856a
Zachary Pataky <zacharypataky@gmail.com>
2022-11-07 11:37:29 -0500

    * Removed redundant component from Assignment 3: Practicing Directives

--- the-basics/basics-assignment-3/src/app/app.module.ts
+++ the-basics/basics-assignment-3/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { AssignmentComponent } from './assignment/assignment.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AssignmentComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
