c029ead999c02fcacc4b784bc67dd8316ee3ad76
Zachary Pataky <zacharypataky@gmail.com>
2022-11-08 20:13:24 -0500

    * Turned the header component into a responsive navbar 	* Turns into a hamburger button when the window is small enough

--- project/src/app/header/header.component.ts
+++ project/src/app/header/header.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  collapsed: boolean = true;
+
   constructor() { }
 
   ngOnInit(): void {
