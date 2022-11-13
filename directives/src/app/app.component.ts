78a9cdf15b1e19c5fb7649e53ecbc5eded713b96
Zachary Pataky <zacharypataky@gmail.com>
2022-11-13 18:31:54 -0500

    * Added starter code for Section 7: Directives Deep Dive

---
+++ directives/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  numbers = [1, 2, 3, 4, 5];
+  onlyOdd = false;
+}
