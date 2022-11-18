005ba0c90b843c77fd8a0bda01e3a75311fee391
Zachary Pataky <zacharypataky@gmail.com>
2022-11-17 20:56:21 -0500

    * Remedied bug in user.component.ts where NgDestroy wasn't properly imported

--- routing/src/app/users/user/user.component.ts
+++ routing/src/app/users/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
 
