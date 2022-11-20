d9fad8747cdb444045c125c21a8d566f6af81c67
Zachary Pataky <zacharypataky@gmail.com>
2022-11-20 18:01:39 -0500

    * Added template-driven form code     * Creating the form and registering its controls     * Submitting and using the form     * Accessing the form with @ViewChild     * Added validation     * Outputted validation errors     * Used ngModel for two-way binding     * Set default values with property-binding     * Group fields with controls     * Added radio buttons     * Patched form fields     * Reset form fields

--- forms/template-driven-forms/src/app/app.component.ts
+++ forms/template-driven-forms/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  @ViewChild('f') signupForm: NgForm;
+  defaultQuestion: string = 'pet';
+  answer: string = '';
+  genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
+
+  // onSubmit(form: NgForm): void {
+  //   console.log(form);
+  // }
+
+  onSubmit() {
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secretQuestion;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
+  }
+
 }
