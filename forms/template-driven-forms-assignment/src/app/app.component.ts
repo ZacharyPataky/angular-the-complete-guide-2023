37686adb2b3f303437e7a709e1e04e0b854af2bf
Zachary Pataky <zacharypataky@gmail.com>
2022-11-20 19:49:10 -0500

    * Completed assignment template-driven-forms-assignment

--- forms/template-driven-forms-assignment/src/app/app.component.ts
+++ forms/template-driven-forms-assignment/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  @ViewChild('form') form: NgForm;
+  subscriptions: string[] = ['Basic', 'Advanced', 'Pro'];
+  defaultSubscription: string = "advanced";
+  submitted = false;
+
+  results = {
+    emailAddress: '',
+    password: '',
+    subscription: ''
+  };
+
+  onSubmit(): void {
+    console.log(this.form);
+
+    this.submitted = true;
+    this.results.emailAddress = this.form.value.email;
+    this.results.password = this.form.value.password;
+    this.results.subscription = this.form.value.subscription;
+
+    this.form.reset();
+  }
 
 }
