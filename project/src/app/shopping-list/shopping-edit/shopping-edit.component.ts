a9ab71ec1c6fc584aa42d8aced203c1042253028
Zachary Pataky <zacharypataky@gmail.com>
2022-12-01 20:06:36 -0500

    * Added a template form to ShoppingEditComponent 	* Replaced original ViewChilds * Added validation to template form of ShoppingEditComponent

--- project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 
@@ -9,9 +10,6 @@ import { ShoppingListService } from '../shopping-list.service';
 })
 export class ShoppingEditComponent implements OnInit {
 
-  @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
-  @ViewChild('amountInput', { static: false }) amountInputRef: ElementRef;
-
   constructor(
     private shoppingListService: ShoppingListService
   ) { }
@@ -20,10 +18,9 @@ export class ShoppingEditComponent implements OnInit {
     // Empty
   }
 
-  onAddItem() {
-    const ingName = this.nameInputRef.nativeElement.value;
-    const ingAmount = this.amountInputRef.nativeElement.value;
-    const newIngredient = new Ingredient(ingName, ingAmount);
+  onAddItem(form: NgForm) {
+    const value = form.value;
+    const newIngredient = new Ingredient(value.name, value.amount);
     this.shoppingListService.addIngredient(newIngredient);
   }
 
