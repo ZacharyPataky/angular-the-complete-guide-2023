d8f3fa8097d46bbcd01e7f6fb26955ecfcc0dfad
Zachary Pataky <zacharypataky@gmail.com>
2022-12-06 18:08:40 -0500

    * Added new directory and the first topic's starter code for Section 18: Making HTTP Requests

---
+++ making-http-requests/http-01/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  loadedPosts = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {}
+
+  onCreatePost(postData: { title: string; content: string }) {
+    // Send Http request
+    console.log(postData);
+  }
+
+  onFetchPosts() {
+    // Send Http request
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+}
