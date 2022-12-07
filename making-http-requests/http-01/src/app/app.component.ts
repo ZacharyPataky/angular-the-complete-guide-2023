0181c0a87a0764019be35212a74078e6cc9a88c3
Zachary Pataky <zacharypataky@gmail.com>
2022-12-06 19:14:35 -0500

    * Added code pertaining to transforming data requests from Section 18   * Transforming POST/GET request responses   * Using types

--- making-http-requests/http-01/src/app/app.component.ts
+++ making-http-requests/http-01/src/app/app.component.ts
@@ -1,23 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { map } from 'rxjs/operators';
+
+import { Post } from './post.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts: Post[] = [];
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     // Send Http request
     console.log(postData);
 
-    this.http.post(
+    this.http.post<{ name: string }>(
       'https://ng-complete-guide-8fc7c-default-rtdb.firebaseio.com/posts.json',
       postData
     ).subscribe(responseData => {
@@ -26,7 +32,24 @@ export class AppComponent implements OnInit {
   }
 
   onFetchPosts() {
-    // Send Http request
+    this.fetchPosts();
+  }
+
+  private fetchPosts() {
+    this.http.get<{ [key: string]: Post }>('https://ng-complete-guide-8fc7c-default-rtdb.firebaseio.com/posts.json')
+      .pipe(map(responseData => {
+        const postsArray: Post[] = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            postsArray.push({ ...responseData[key], id: key });
+          }
+        }
+        return postsArray;
+      }))
+      .subscribe(posts => {
+        // ...
+        this.loadedPosts = posts;
+      });
   }
 
   onClearPosts() {
---
+++ making-http-requests/http-01/src/app/post.model.ts
@@ -0,0 +1,5 @@
+export interface Post {
+  title: string;
+  content: string;
+  id?: string;
+}
