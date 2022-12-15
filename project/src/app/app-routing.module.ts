6732ea3d8cc89c17842d0b64be012010be85f28f
Zachary Pataky <zacharypataky@gmail.com>
2022-12-15 18:31:44 -0500

    * Added route guards so unauthenticated users can't access the recipes page

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { AuthComponent } from "./auth/auth.component";
+import { AuthGuard } from "./auth/auth.guard";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
 import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
 import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
@@ -11,7 +12,11 @@ import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full'},
-    { path: 'recipes', component: RecipesComponent, children: [
+    {
+      path: 'recipes',
+      component: RecipesComponent,
+      canActivate: [AuthGuard],
+      children: [
         { path: '', component: RecipeStartComponent },
         { path: 'new', component: RecipeEditComponent },
         {
