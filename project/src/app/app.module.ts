b9125aa802e38d2675d303776caed3f553cf4025
Zachary Pataky <zacharypataky@gmail.com>
2023-01-05 19:17:08 -0500

    * npm installed @ngrx/store/devtools * Added StoreDevtoolsModule to AppModule * Fixed AutoLogout bug * npm installed @ngrx/router-store

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -7,10 +7,15 @@ import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
+
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+
 import * as fromApp from './store/app.reducer';
 import { AuthEffects } from './auth/store/auth.effects';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -24,7 +29,11 @@ import { AuthEffects } from './auth/store/auth.effects';
     SharedModule,
     CoreModule,
     StoreModule.forRoot(fromApp.appReducer),
-    EffectsModule.forRoot([AuthEffects])
+    EffectsModule.forRoot([AuthEffects]),
+    StoreDevtoolsModule.instrument({
+      logOnly: environment.production
+    }),
+    StoreRouterConnectingModule.forRoot()
   ],
   bootstrap: [AppComponent]
 })
--- project/src/app/auth/store/auth.effects.ts
+++ project/src/app/auth/store/auth.effects.ts
@@ -144,7 +144,7 @@ export class AuthEffects {
         }
       ).pipe(
         tap(resData => {
-          this.authService.setLogoutTimer(+resData.expiresIn);
+          this.authService.setLogoutTimer(+resData.expiresIn * 1000);
         }),
         map(resData => {
           return handleAuthentication(
