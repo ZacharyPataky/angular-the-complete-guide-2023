57bb641f7e002224de37079434b30d03c196d18a
Zachary Pataky <zacharypataky@gmail.com>
2023-01-07 20:26:27 -0500

    * Added addition/removal animations for lists with void state

--- animations/src/app/app.component.ts
+++ animations/src/app/app.component.ts
@@ -42,6 +42,25 @@ import { Component } from '@angular/core';
         })),
         animate(500)
       ])
+    ]),
+    trigger('list1', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void => *', [
+        style({
+          opacity: 0,
+          transform: 'translateX(-100px)'
+        }),
+        animate(300)
+      ]),
+      transition('* => void', [
+        animate(300, style({
+          transform: 'translateX(100px)',
+          opacity: 0
+        }))
+      ])
     ])
   ]
 })
