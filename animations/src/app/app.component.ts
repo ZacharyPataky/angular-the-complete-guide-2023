165cc5d32bf66609ab0fbc04f4fe6b71cf865a5a
Zachary Pataky <zacharypataky@gmail.com>
2023-01-07 20:16:29 -0500

    * Added a complex transition with another box that shrinks and changes colors

--- animations/src/app/app.component.ts
+++ animations/src/app/app.component.ts
@@ -14,13 +14,41 @@ import { Component } from '@angular/core';
         backgroundColor: 'blue',
         transform: 'translateX(100px)'
       })),
+      transition('normal <=> highlighted', animate(300)),
+      // transition('highlighted => normal', animate(800))
+    ]),
+    trigger('wildState', [
+      state('normal', style({
+        backgroundColor: 'red',
+        transform: 'translateX(0) scale(1)'
+      })),
+      state('highlighted', style({
+        backgroundColor: 'blue',
+        transform: 'translateX(100px) scale(1)'
+      })),
+      state('shrunken', style({
+        backgroundColor: 'green',
+        transform: 'translateX(0px) scale(0.5)'
+      })),
+      // transition('normal <=> highlighted', animate(300)),
       transition('normal => highlighted', animate(300)),
-      transition('highlighted => normal', animate(800))
+      transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', [
+        style({
+          backgroundColor: 'orange'
+        }),
+        animate(1000, style({
+          borderRadius: '50px'
+        })),
+        animate(500)
+      ])
     ])
   ]
 })
 export class AppComponent {
   state = 'normal';
+  wildState = 'normal';
+
   list = ['Milk', 'Sugar', 'Bread'];
 
   onAdd(item) {
@@ -33,5 +61,10 @@ export class AppComponent {
 
   onAnimate() {
     this.state === 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+    this.wildState === 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
+  }
+
+  onShrink() {
+    this.wildState = 'shrunken';
   }
 }
